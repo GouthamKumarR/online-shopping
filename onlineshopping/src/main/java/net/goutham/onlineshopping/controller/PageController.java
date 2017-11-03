1001c43ae845b068fc18e69821f86c7418fc57b5
Goutham <gouthamkum1993@gmail.com>
2017-11-03 17:59:52 +0530

    Sucessfully add static resource

---
+++ onlineshopping/src/main/java/net/goutham/onlineshopping/controller/PageController.java
@@ -0,0 +1,38 @@
+package net.goutham.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/" , "/index" , "/home"})
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to spring web mvc");
+		return mv;
+		
+	}
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value ="greeting", required=false)String greeting){
+		if(greeting == null){
+			greeting = "Static value";
+		}
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("greeting", greeting);
+			return mv;
+	}*/
+	
+	/*@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting){
+		if(greeting == null){
+			greeting = "Static value";
+		}
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("greeting", greeting);
+			return mv;
+	}*/
+
+}
