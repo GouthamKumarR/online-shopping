1b15284f8e27afb21b1e0b03f3d88bcd70d2b5d4
Goutham <gouthamkum1993@gmail.com>
2017-11-06 12:08:31 +0530

    Completed inface and theme

--- onlineshopping/src/main/java/net/goutham/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/goutham/onlineshopping/controller/PageController.java
@@ -10,29 +10,77 @@ public class PageController {
 	@RequestMapping(value= {"/" , "/index" , "/home"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to spring web mvc");
+		/*mv.addObject("greeting", "Welcome to spring web mvc");*/
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 		
 	}
 	
-	/*@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value ="greeting", required=false)String greeting){
-		if(greeting == null){
-			greeting = "Static value";
-		}
-			ModelAndView mv = new ModelAndView("page");
-			mv.addObject("greeting", greeting);
-			return mv;
-	}*/
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");
+		/*mv.addObject("greeting", "Welcome to spring web mvc");*/
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+		
+	}
 	
-	/*@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting){
-		if(greeting == null){
-			greeting = "Static value";
-		}
-			ModelAndView mv = new ModelAndView("page");
-			mv.addObject("greeting", greeting);
-			return mv;
-	}*/
-
+	
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		/*mv.addObject("greeting", "Welcome to spring web mvc");*/
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true); 
+		return mv;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*@RequestMapping(value="/test")
+public ModelAndView test(@RequestParam(value ="greeting", required=false)String greeting){
+	if(greeting == null){
+		greeting = "Static value";
+	}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+}*/
+
+/*@RequestMapping(value="/test/{greeting}")
+public ModelAndView test(@PathVariable("greeting")String greeting){
+	if(greeting == null){
+		greeting = "Static value";
+	}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+}*/
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,27 @@
+$(function(){
+	
+	switch(menu){
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		$('#home').removeClass('active');
+		break;
+		
+		case 'Contact Us':
+			$('#contact').addClass('active');
+			$('#home').removeClass('active');
+			break;
+		
+		default:
+				$('#home').addClass('active');
+				break;
+		
+	
+	
+	
+	}
+
+
+
+});
+
